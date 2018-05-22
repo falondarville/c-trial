5ae13df9a136bcfd3681afa04bfc33adac232c6d
Falon Darville <falondarville@gmail.com>
2018-05-22 16:12:35 -0700

    use scan

---
+++ scan.c
@@ -0,0 +1,28 @@
+// standard input and output
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+	char middleInitial;
+
+	printf("What is your middle name?");
+
+	// receive input from our users
+	// the second argument tells C where to save the variable that the user has inputted
+	// note that you are telling the program with %c what type of data you are getting. You need to include a space before you define this within quotation marks.
+	// if the user inputs more than one character, or strings with a space between them, the information would be ignored. 
+	// When I put in a full name, it cut off everything after the first letter
+	scanf(" %c", &middleInitial);
+
+	// prints the user input that was saved to the middleInitial variable
+	printf("Your middle initial is %c\n\n", middleInitial);
+
+	char firstName[30], lastName[30];
+
+	printf("What is your name?");
+
+	scanf(" %s %s", firstName, lastName);
+
+	printf("Your name is %s %c %s\n\n", firstName, middleInitial, lastName);
+}
--- test.c
+++ test.c
@@ -2,10 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-	printf("Hello World!\n");
-}
-
 // creates a constant. It is not a requirement to put this variable in all caps, but C developers generally do it this way
 #define MYNAME "Falon Darville"
 
@@ -13,7 +9,8 @@ int main(){
 int globalVar = 100;
 
 // where the computer starts executing your code
-main(){
+// when you do not define the type, it will default to int, but it still throws me an warning in terminal. Note that this does not prevent me from compiling and running the code.
+int main(){
 
 	// characters must be surrounded by single quotes
 	char firstLetter = 'D';
@@ -27,7 +24,44 @@ main(){
 	long int superBigNum = -32678000;
 
 	// use float if your number contains decimals
+	// a float can be as big as 38 decimal points in length
 	float piValue = 3.14159;
 
+	// use for larger numbers with decimal point
+	double reallyLargeNum = 3.1415914159141591415914159;
+
+	// new line 
+	printf("\n");
+
+	printf("This will print to screen\n\n");
+
+	// use %d and pass through the variable that you would like to populate there. 
+	// %d means print an integer
+	printf("I am %d years old\n\n", age);
+
+	// this prints large numbers
+	printf("Big Number = %ld\n\n", superBigNum);
+
+	// print a float with %.5f and add the number of decimal places that you would like appear instead of the 5. In the following, we will be printing 5 decimal places.
+	printf("Pi = %.5f\n\n", piValue);
+
+	// to print doubles, use %f as well. 
+	printf("Big float = %.15f\n\n", reallyLargeNum);
+
+	// print characters
+	printf("The first letter in my last name is %c\n\n", firstLetter);
+
+	// print a string
+	printf("My name is %s\n\n", "Falon");
+
+	// print a string by creating an array
+	// this is called a character array
+	char myName[] = "Falon Darville";
+
+	// string copy allow you to change the value of this string array
+	// the reason that we can do the following is because of #include <string.h> at the top of the file
+	strcpy(myName, "Bob Smith");
+
+	printf("My name is %s\n\n", myName);
 }
 
