b652a61c3b0ed8f20ecf4b1c5bb6c457b78315c3
Falon Darville <falondarville@gmail.com>
2018-05-22 16:24:34 -0700

    add math file

---
+++ math.c
@@ -0,0 +1,16 @@
+// standard input and output
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+	int num1 = 12, num2 = 15, numAns;
+
+	float decimal1 = 1.2, decimal2 = 1.5, decimalAns;
+
+	printf("Integer Calculation %d\n\n", num2 / num1);
+
+	printf("Float Calculation %f\n\n", decimal2 / decimal1);
+
+	printf("Modulus %d\n\n", num2 % num1);
+}
--- scan.c
+++ scan.c
@@ -25,4 +25,12 @@ int main(){
 	scanf(" %s %s", firstName, lastName);
 
 	printf("Your name is %s %c %s\n\n", firstName, middleInitial, lastName);
+
+	int month, day, year;
+
+	printf("What is your birth date?");
+
+	scanf(" %d/%d/%d", &month, &day, &year);
+
+	printf("\nBirth Date %d/%d/%d\n\n", month, day, year);
 }
